f214655c75c96f7648fed90aece488e5ed2155b8
Nazhira Sabila <fathanalghifari@gmail.com>
2026-03-02 15:32:11 +0700

    latihan

--- ti_design_pattern/src/pratikum_1/latihan/LatihanPerulangan.java
+++ ti_design_pattern/src/pratikum_1/latihan/LatihanPerulangan.java
@@ -1,4 +1,9 @@
 package pratikum_1.latihan;
 
 public class LatihanPerulangan {
+    public static void main(String[] args){
+        for(int i = 1; i <= 20; i+=2){
+            System.out.println(i);
+        }
+    }
 }
