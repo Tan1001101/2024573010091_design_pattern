282f559cf7b28e0d70b05ffd8c2b26505d84e974
Nazhira Sabila <fathanalghifari@gmail.com>
2026-03-02 10:48:31 +0700

    quick review - pemrograman java dasar

---
+++ ti_design_pattern/src/modul_1/Latihan.java
@@ -0,0 +1,29 @@
+package modul_1;
+
+public class Latihan {
+        public static void main(String[] args){
+            int umur = 19;
+            double tinggi = 1.75;
+            boolean isMahasiswa = true;
+            char jenisKelamin = 'Laki - laki';
+            String nama  = "Fathan Al Ghifari";
+            String tempatTinggal = "Lhokseumawe";
+            String tanggalLahir = "24 agustus 2006";
+            char golonganDarah = '-';
+            String agama = "islam";
+            String pekerjaan = "mahasiswa";
+
+            System.out.println("Nama Lengkap: " + nama);
+            System.out.println("Umur: " + umur);
+            System.out.println("Tempat Tinggal: " + tempatTinggal);
+            System.out.println("Tanggal Lahir: " + tanggalLahir);
+            System.out.println("Golongan darah: " + golonganDarah);
+            System.out.println("Tinggi: " + tinggi + " m");
+            System.out.println("Mahasiswa: " + isMahasiswa);
+            System.out.println("Jenis Kelamin: " + jenisKelamin);
+            System.out.println("Agama: " + agama);
+            System.out.println("Pekerjaan: " + pekerjaan);
+
+        }
+    }
+
---
+++ ti_design_pattern/src/modul_1/Variable.java
@@ -0,0 +1,18 @@
+package modul_1;
+
+public class Variable {
+    public static void main(String[] args){
+        int umur = 19;
+        double tinggi = 1.75;
+        boolean isMahasiswa = true;
+        char jenisKelamin = 'L';
+        String nama = "Fathan Al Ghifari";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi + "m");
+        System.out.println("Mahasiswa: " + isMahasiswa);
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+
+    }
+}
